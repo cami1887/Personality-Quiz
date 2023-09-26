74d734177f987c772a54bd42b4cfe43168a44325
Camille Lacaden <cami1887@gmail.com>
2023-09-25 21:49:00 -0700

    finished sidebar

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -7,7 +7,8 @@ export default function Posts() {
         {username: 'injail_outsoon', post: 'Shut up.'},
         {username: 'vlogger_mommy1023', post: 'Hey everyone! Just released a new video: PRANKED MY NEWBORN | GROSS on my YT. Support me in the comments :D'},
         {username: 'egghead_007', post: 'egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg egg eggg ggggggggggg'},
-        {username: 'PPpoopp', post: 'hi evryone'}
+        {username: 'PPpoopp', post: 'hi evryone'},
+        {username: 'lorne_a_20014', post: '36m nashville'}
     ]
 
     return (
--- src/pages/sidebar.js
+++ src/pages/sidebar.js
@@ -1,29 +1,20 @@
 import '../styling/sidebar.css';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import CloseIcon from '@mui/icons-material/Close';
-// import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
-    // const [clicked, showMenu] = useState(false);
-    // function ToggleMenu() {
-    //     showMenu(!clicked);
-    // }
     return (
-        <>
-        {/* <MenuIcon className='menu-icon' onClick={ToggleMenu} style={{visibility: clicked ? 'hidden' : 'visible'}}/> */}
-        <img className='menu-icon'  src="/images/egg-icon.png" alt="open navigation" /> 
-        {/*style=visibility: clicked ? 'hidden' : 'visible'*/}
-        <nav className="sidebar"> 
-        {/*style=visibility: clicked ? 'visible' : 'hidden' */}
-          {/* <CloseIcon className="close-icon" onClick={ToggleMenu}/> */}
-          <div className="navlink-container" >  
-            <NavLink className="navlink" to="/">Home</NavLink>
-            <NavLink className="navlink" to="/loserboard">Top 10 Worst People</NavLink>
-            <NavLink className="navlink" to="/twelve-steps">12 Step Program</NavLink>
-            <NavLink className="navlink" to="/forum">Community</NavLink>
-          </div>
-        </nav>
-        </>
+      <>
+          <img className='menu-icon'  src="/images/egg-icon.png" alt="open navigation" /> 
+          <nav className="sidebar"> 
+            <div className="sidebar-background"></div>
+            <img className="fried-egg" src="/images/friedegg.png" alt="sidebar" />
+            <div className="navlink-container" >  
+              <NavLink className="navlink" to="/">Home</NavLink>
+              <NavLink className="navlink" to="/loserboard">Top 10 Worst People</NavLink>
+              <NavLink className="navlink" to="/twelve-steps">12 Step Program</NavLink>
+              <NavLink className="navlink" to="/forum">Community</NavLink>
+            </div>
+          </nav>
+      </>
     )
 }
